7b61341d93c8f024027e30229723be639cb2b013
Timur Alhoev <timuralhoev@yandex.ru>
2023-06-17 22:18:08 +0300

    XX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [rates, setRates] = React.useState({});
+  React.useEffect;
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block
+        value={0}
+        currency="RUB"
+        onChangeCurrency={(cur) => console.log(cur)}
+      />
       <Block value={0} currency="USD" />
     </div>
   );
